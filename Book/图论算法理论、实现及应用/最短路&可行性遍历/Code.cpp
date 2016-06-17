ee2cba0d4acaf55ca28024cfa1ea22110a0c0374
GooZy <guoziyao@foxmail.com>
2016-06-17 18:46:20 +0800

    update

---
+++ Book/图论算法理论、实现及应用/最短路&可行性遍历/Code.cpp
@@ -0,0 +1,87 @@
+#include<algorithm>
+#include<cctype>
+#include<cmath>
+#include<cstdio>
+#include<cstring>
+#include<iomanip>
+#include<iostream>
+#include<map>
+#include<queue>
+#include<set>
+#include<sstream>
+#include<stack>
+#include<string>
+#define ll long long
+#define pr(x) cout << #x << " = " << (x) << '\n';
+using namespace std;
+
+const int INF = 0x7f7f7f7f;
+const int MAXN = 1e5 + 111;
+
+struct Edge {
+    int v, nxt;
+}edge[MAXN * 10];
+int head[MAXN], tol, mx, mod;
+bool vis[MAXN * 10];
+int sta[MAXN * 10], top, sum, n;
+
+// 结点代表的是长度为n - 1的状态
+void addedge(int u, int v) {
+    edge[tol].v = v;
+    edge[tol].nxt = head[u];
+    head[u] = tol++;
+}
+
+void dfs(int s) {
+    top = 0;
+    //int ok = 0;
+    sta[top++] = s;
+    while (1) {
+        //ok = max(top, ok); pr(ok)
+        // 最后的n - 1个0和最后的n - 2个9是不会到达的状态，因为除mod取第一个数字的关系
+        if (top == sum + n - 1 - (2 * n - 3)) break;
+        int u = sta[top - 1], i;
+        for (i = head[u]; ~i; i = edge[i].nxt) {
+            int v = edge[i].v;
+            if (!vis[i]) {
+                vis[i] = 1;
+                sta[top++] = v;
+                break;
+            }
+        }
+        if (i == -1) {
+            --top;
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    while (cin >> n && n) {
+        if (n == 1) {
+            cout << "0123456789\n";
+            continue;
+        }
+        memset(head, -1, sizeof head);
+        memset(vis, 0, sizeof vis);
+        tol = 0;
+        mx = 1;
+        for (int i = 1; i < n; ++i) mx *= 10; // pow(10, n - 1) 有精度误差我去= =
+        sum = mx * 10;
+        mod = mx / 10;
+        for (int i = 0; i < mx; ++i) {
+            for (int j = 9; j >= 0; --j) {
+                addedge(i, (i % mod) * 10 + j); // 链式前向星，这样字典序最小
+            }
+        }
+        dfs(0);
+        for (int i = 0; i < top - 1; ++i) {
+            cout << sta[i] / mod;
+        }
+        cout << sta[top - 1];
+        for (int i = 1; i < n; ++i) cout << 0;
+        cout << '\n';
+    }
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,107 +1,80 @@
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-#include<queue>
-
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <algorithm>
 using namespace std;
 
-const int VM=30;
-const int INF=0x3f3f3f3f;
+int N, mod, head[100005];
+int idx, lim, edge, top;
+int stk[1000100];
 
-struct Edge{
-    int to,nxt;
-    int cap;
-}edge[2010];
+struct Edge {
+    int v, next;
+}e[1000100]; // 每个节点只能够引出10条边
 
-int n,cnt,head[VM],ans;
-int need[VM],have[VM],dis[VM],Count[VM],vis[VM];    //// 每个节点的dis[i]表示的含义为到第 i 个小时时间段已经雇佣的员工数量
-  //need[i]表示 i 时间段一共雇佣了多少员工    have[i]表示 i 时段有多少人申请工作
+char vis[1000100];
 
-void addedge(int cu,int cv,int cw){
-    edge[cnt].to=cv;    edge[cnt].cap=cw;   edge[cnt].nxt=head[cu];
-    head[cu]=cnt++;
+void insert(int a, int b) {
+    e[idx].v = b;
+    e[idx].next = head[a];
+    head[a] = idx++;
 }
 
-void build(int lim){    // lim表示欲招的员工个数
-    cnt=0;
-    memset(head,-1,sizeof(head));
-    for(int i=1;i<=24;i++){
-        addedge(i-1,i,have[i]);     // need[i] - need[i-1] <= hav[i]， 雇佣的人数少于申请者
-        addedge(i,i-1,0);           // need[i-1] - need[i] <= 0   即  need[i] - need[i-1] >= 0   每个小时内雇佣的人数应该是个非负数
-    }
-    for(int i=1;i<8;i++)    // 由于无法直接得到从前一天的后几小时到当天前几小时的雇佣人数，所以反面求解
-        addedge(i,24+i-8,lim-need[i]);  // need[24+i-8] - need[i] <= lim-need[i]
-    for(int i=8;i<=24;i++)  // 前8小时内雇佣人数应该大于等于需求量
-        addedge(i,i-8,-need[i]);    // need[i] - need[i-8] >= need[i] ,即：need[i-8]-need[i]<=-need[i]
-    addedge(24,0,-lim); // need[24] - need[0] >= lim,即：need[0]-need[24]<=-lim  表示至少为lim个员工，这样就能够看在最好情况下能不能取到这个最小值
-}
-
-int SPFA(int lim){
-    queue<int> q;
-    while(!q.empty())
-        q.pop();
-    for(int i=0;i<VM;i++){
-        dis[i]=INF;
-        vis[i]=0;
-        Count[i]=0;
-    }
-    dis[24]=0;  vis[24]=1;  Count[24]=1;
-    q.push(24);
-    while(!q.empty()){
-        int u=q.front();
-        q.pop();
-        if(Count[u]>25)
-            return 0;
-        vis[u]=0;
-        for(int i=head[u];i!=-1;i=edge[i].nxt){
-            int v=edge[i].to;
-            if(dis[v]>dis[u]+edge[i].cap){
-                dis[v]=dis[u]+edge[i].cap;
-                if(!vis[v]){
-                    vis[v]=1;
-                    Count[v]++;
-                    q.push(v);
-                }
+void dfs() {
+    stk[top++] = 0;
+    while (1) {
+        int flag = 0;
+        int v = stk[top-1]; // 取出栈顶元素
+        if (top == edge-(N-2)) { // 由于遍历的是点，因此从一个点N个9到N个0最终无法到达，中间也就少了N-2个状态
+            return;
+        }
+        for (int i = head[v]; i != -1; i = e[i].next) {
+            if (!vis[i]) {
+                flag = 1;
+                vis[i] = 1;
+                //if (top < 4) printf("%d\n", e[i].v);
+                stk[top++] = e[i].v;
+                break;
             }
         }
-    }
-    return dis[0]=-lim;
-}
-
-int BinSearch(int l,int r){
-    ans=-1;
-    while(l<=r){
-        int mid=(l+r)>>1;   // mid 表示一共能够雇佣的员工数量
-        build(mid);
-        if(SPFA(mid)){
-            ans=mid;
-            r=mid-1;    //因为此题要求的是最小的雇佣人数，所以得判断r=mid-1使否满足题意，使之越来越小
-        }else
-            l=mid+1;
-    }
-    return ans!=-1;
-}
-
-int main(){
-
-    //freopen("input.txt","r",stdin);
-
-    int t;
-    scanf("%d",&t);
-    while(t--){
-        memset(have,0,sizeof(have));
-        for(int i=1;i<=24;i++)  //0时刻申请将工作在第一个小时
-            scanf("%d",&need[i]);
-        scanf("%d",&n);
-        int x;
-        for(int i=0;i<n;i++){
-            scanf("%d",&x);
-            have[x+1]++;    //这样做是为了从1～24计算
+        if (!flag) {
+            --top;
         }
-        if(BinSearch(0,n))
-            printf("%d\n",ans);
-        else
-            printf("No Solution\n");
+    }
+}
+
+int main() {
+    while (scanf("%d", &N), N) {
+        if (N == 1) {
+            printf("0123456789\n");
+            continue;
+        }
+        idx = top = 0;
+        memset(head, 0xff, sizeof (head));
+        memset(vis, 0, sizeof (vis));
+        lim = 1;
+        for (int i = 1; i < N; ++i) {
+            lim *= 10;
+        }
+        edge = lim * 10;
+        mod = lim / 10; // mod用来提取后后几位
+        printf("%d\n", mod);
+        for (int i = 0; i < lim; ++i) {
+            for (int j = 9; j >= 0; --j) { // 十个状态转移
+                insert(i, (i%mod)*10+j);
+            }
+        }
+        dfs();
+        for (int i = 0; i < top-1; ++i) {
+            //if (i < 5) printf("\n test = %d \n", stk[i]);
+            printf("%d", stk[i]/mod);
+        }
+        printf("%d", stk[top-1]);
+        for (int i = 1; i < N; ++i) {
+            printf("0");
+        }
+        puts("");
     }
     return 0;
 }
