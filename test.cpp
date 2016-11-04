5d59d23f01dda08ca38970fdfec0950e70d7641d
GooZy <ziyaoguo@gmail.com>
2016-11-04 22:30:16 +0800

    update

--- test.cpp
+++ test.cpp
@@ -1,32 +1,29 @@
-#include<algorithm>
-#include<bitset>
-#include<cctype>
-#include<cmath>
-#include<cstdio>
-#include<cstring>
-#include<iomanip>
-#include<iostream>
-#include<map>
-#include<queue>
-#include<set>
-#include<sstream>
-#include<stack>
-#include<string>
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
 #define ll long long
 #define pr(x) cout << #x << " = " << (x) << " ; ";
 #define prln(x) cout << #x << " = " << (x) << '\n';
 using namespace std;
 
 const int INF = 0x7f7f7f7f;
-const int MAXN = 1e6 + 111;
+const int MAXN = 1e5 + 111;
 
 int main()
 {
     #ifdef GooZy
-    freopen("C:\\Users\\apple\\Desktop\\in.txt", "r", stdin);
+    freopen("/Users/apple1/Desktop/in.txt", "r", stdin);
+    //freopen("/Users/apple1/Desktop/out.txt","w",stdout);
     #endif
-    int a, b;
-    scanf("%d*%d", &a, &b);
-    cout << a * b << '\n';
     return 0;
 }
