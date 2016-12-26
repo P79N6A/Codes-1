3d58f4945c6063253b57195a52c04986d1ccdf8c
GooZy <ziyaoguo@gmail.com>
2016-12-26 22:12:28 +0800

    update shell

--- upload.sh
+++ upload.sh
@@ -1,1 +1,15 @@
-git add -A
+#!/bin/bash
+if [ $1 == "1" ]
+then
+    git add -A
+fi
+
+if [ "$2" != "0" ]
+then
+    git commit -m "$2"
+fi
+
+if [ $3 == "1" ]
+then
+    git push origin master
+fi
