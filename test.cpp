16450b76f9c624bec0c4f46d8fa30897a1c71501
GooZy <ziyaoguo@gmail.com>
2017-01-13 11:03:00 +0800

    update

--- test.cpp
+++ test.cpp
@@ -16,48 +16,35 @@
 #define prln(x) cout << #x << " = " << (x) << '\n';
 using namespace std;
 
-const int INF = 0x3f3f3f3f;
-const int MAXN = 1e3 + 111;
+const int INF = 0x7f7f7f7f;
+const int MAXN = 1e5 + 111;
 
-int a[MAXN];
-int dp[MAXN][MAXN], sum[MAXN];
+const double eps = 1e-6;
+const double PI = acos(-1);
 
-int dfs(int i, int j) {
-    int &ret = dp[i][j];
-    if (ret != -1) return ret;
-    if (i == j) return ret = 0;
-    ret = INF;
-    for (int k = i; k < j; ++k) {
-        ret = min(ret, dfs(i, k) + dfs(k + 1, j) + sum[j] - sum[i - 1]);
-    }
-    return ret;
+bool judge(double p, double x, double y) {
+    if (x * x + y * y - 50 * 50 > eps) return true;
+    double x1 = 0, y1 = 50;
+    double x2 = x - 50, y2 = y - 50;
+    double cs = (x1 * x2 + y1 * y2) / (sqrt(x1 * x1 + y1 * y1) * sqrt(x2 * x2 + y2 * y2));
+    double cur = acos(cs);
+    if (y < 50) cur = 2 * PI - cur;
+    p = 2 * PI * p / 100;
+    return cur > p + eps;
 }
 
 int main()
 {
     #ifdef GooZy
     freopen("/Users/apple1/Desktop/in.txt", "r", stdin);
-    //freopen("/Users/apple1/Desktop/in.txt","w",stdout);
+    freopen("/Users/apple1/Desktop/out.txt","w",stdout);
     #endif
-    string s, ret;
-    getline(cin, s);
-    for (int i = 0; i < s.length(); ++i) {
-        if (s[i] != ' ') ret += s[i];
-    }
-    cout << ret << '\n';
-
-    int n; cin >> n;
-    for (int i = 1; i <= n; ++i) {
-        cin >> a[i];
-        sum[i] = sum[i - 1] + a[i];
-    }
-    memset(dp, -1, sizeof dp);
-    cout << dfs(1, n) << '\n';
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= n; ++j) {
-            cout << dp[i][j] << ' ';
-        }
-        cout << '\n';
+    int t; cin >> t;
+    double p, x, y;
+    for (int kase = 1; kase <= t; ++kase) {
+        cin >> p >> x >> y;
+        bool flag = judge(p, x, y);
+        cout << "Case #" << kase << ": " << (flag? "white" : "black") << '\n';
     }
     return 0;
 }
