afeb7ea32388085e0f15f14e8407de7691002dc1
郭子尧 <guoziyao@bytedance.com>
2017-08-15 21:21:04 +0800

    feat: https://harrynull.tech/cipher/

---
+++ cipher/Lv7.py
@@ -0,0 +1,14 @@
+from PIL import Image, ImageFilter
+# 读取图像
+im = Image.open( 'blind.png' )
+w, h = im.size
+# 处理
+f = open('lv7_result.txt', 'w')
+for i in range(0,w):
+    for j in range(h - 1, -1, -1):
+        r, g, b, a = im.getpixel((i,j))
+        if b < 255:
+            f.write('-')
+        else:
+            f.write('*')
+    f.write('\n')
