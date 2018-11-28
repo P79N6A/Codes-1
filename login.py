cf8d19755cf6580759947a2798a85c1e49720183
GooZy <ziyaoguo@gmail.com>
2018-11-28 17:18:05 +0800

    feat: update

--- login.py
+++ login.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import subprocess
+from collections import OrderedDict
 
 
 YAO_LOGIN = 'ssh guoziyao@%s'
 TIGER_LOGIN = 'ssh tiger@%s'
 
-name_list = ['dev', 'dev2', 'pyspider0', 'pyspider1', 'metis_test', 'flow0', 'flow1', 'selenium']
-
-ip_address = {
-    'dev': '10.6.131.79',
-    'dev2': '10.8.127.161',
-    'pyspider0': '10.6.15.150',
-    'pyspider1': '10.6.15.151',
-    'metis_test': '10.6.15.137',
-    'flow0': '10.12.22.137',
-    'flow1': '10.12.22.133',
-    'selenium': '10.15.63.98',
-}
+ip_address = OrderedDict({
+    'dev': '',
+    'dev2': '',
+    'pyspider00': '',
+    'pyspider01': '',
+    'pyspider10': '',
+    'pyspider11': '',
+    'flow0': '',
+    'flow1': '',
+    'selenium': '',
+})
 
 
 def login(target):
@@ -29,7 +29,9 @@ def login(target):
 
 if __name__=='__main__':
     print('Plz choose your target machine.')
-    for index, each in enumerate(name_list):
-        print('%d: %s = %s' %(index, each, ip_address[each]))
+    name_list = list()
+    for k, v in ip_address.items():
+        print('%d: %s = %s' %(len(name_list), k, v))
+        name_list.append(k)
     x = input()
     login(name_list[int(x)])
