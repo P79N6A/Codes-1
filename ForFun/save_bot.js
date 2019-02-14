8169c140408ed3689aa4af84f5fc79b9d991ed72
GooZy <ziyaoguo@gmail.com>
2019-02-14 20:03:24 +0800

    feat: update

---
+++ ForFun/save_bot.js
@@ -0,0 +1,19 @@
+/**
+ * Created by guoziyao on 2019/2/2.
+ * 购物省心助手bot
+ */
+/**
+ * 慢慢买
+ *
+ * https://item.jd.com/8894295.html
+ *
+ * 有历史数据，但是价格不规范
+ * https://bijiatool-v2.manmanbuy.com/ChromeWidgetServices/WidgetServices.ashx?jsoncallback=&methodName=getZhekou&p_url={}
+ *
+ * 历史数据，完备，需要token
+ * http://tool.manmanbuy.com/history.aspx?DA=1&action=gethistory&url={}&spm=a21bo.2018.2001.8.5af911d9H5XshR&bjid=&spbh=&cxid=&zkid=&w=951&token=y4es015f32d719c9886052023172492289bf4n9e2
+ *
+ * 有历史数据，还有趋势，但是没有最近的。。。
+ * https://bijiatool-v2.manmanbuy.com/ChromeWidgetServices/WidgetServices.ashx?jsoncallback=&methodName=getBiJiaInfo_wxsmall&p_url={}
+ *
+ */
